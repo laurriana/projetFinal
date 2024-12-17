3c2aa293c14b13226f0951a9f99c9672c025d2ed
lauriana.raspberry <laurianalouis@gmail.com>
2024-12-17 18:56:09 -0500

    hopefully my last commit Please.

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ def main():
     time.sleep(1)
     print("please enter your codes.")
     
-    #read_function = threading.Thread(target=service.read_button, args=(stop_event,), daemon=True)
-    #read_function.start()
-    while stop_event == False:
-        service.read_button()
-        user_codes = service.creation_code()
-        stop_event = True
-        print(f"your code is: {user_codes}")
+    read_function = threading.Thread(target=service.read_button, args=(stop_event,), daemon=True)
+    read_function.start()
 
-        time.sleep(2)
+    user_codes = service.creation_code()
+    stop_event = True
+    print(f"your code is: {user_codes}")
 
-        print("it is now time to unlock your lock!")
-        attempt_codes = service.enter_code()
-        print(f"you wrote: {attempt_codes}")
+    time.sleep(2)
 
+    print("it is now time to unlock your lock!")
+    attempt_codes = service.enter_code()
+    print(f"you wrote: {attempt_codes}")
+    
     service.compare_codes(user_codes, attempt_codes)
 
 if __name__ == "__main__":
